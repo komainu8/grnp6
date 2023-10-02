0d5e6e7feee8089946b8979d413680ecd8b4b5da
Horimoto Yasuhiro <horimoto@clear-code.com>
2023-10-02 21:56:44 +0900

    ci: extract common process

--- t/setup.sh
+++ t/setup.sh
@@ -2,7 +2,4 @@
 
 set -xue
 
-wget https://rakudo.org/dl/rakudo/rakudo-moar-$1-linux-x86_64-gcc.tar.gz
-tar -zxf rakudo-moar-$1-linux-x86_64-gcc.tar.gz
-mv rakudo-moar-$1-linux-x86_64-gcc rakudo
-./rakudo/bin/raku --version
+/host/rakudo/bin/raku --version
