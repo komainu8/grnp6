300bc943919361c1aa086ecb789273d4019abad7
Horimoto Yasuhiro <horimoto@clear-code.com>
2023-10-10 21:17:37 +0900

    test: fix a wrong path

--- t/run-tests.sh
+++ t/run-tests.sh
@@ -11,5 +11,5 @@ ls -al /host/rakudo/share/perl6/site
 ls -al /host/rakudo/share/perl6/site/bin
 
 echo "Run tests!"
-/host/rakudo/share/perl6/site/bin/prove6 --lib t/
+/host/rakudo/share/perl6/site/bin/prove6 --lib /host/t/
 echo "Finished tests!"
