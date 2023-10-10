b32c0a61343155233217afdaf49f971b0f7fea81
Horimoto Yasuhiro <horimoto@clear-code.com>
2023-10-10 21:17:15 +0900

    test: output location
    This is for debug.

--- t/run-tests.sh
+++ t/run-tests.sh
@@ -4,7 +4,11 @@ set -xue
 
 pwd
 ls -al /
-ls -al /host
+ls -al /host/rakudo/
+ls -al /host/rakudo/share/
+ls -al /host/rakudo/share/perl6
+ls -al /host/rakudo/share/perl6/site
+ls -al /host/rakudo/share/perl6/site/bin
 
 echo "Run tests!"
 /host/rakudo/share/perl6/site/bin/prove6 --lib t/
