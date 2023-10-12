54c762977d77b08693db9e5a4fccb7576fa6f6c2
Horimoto Yasuhiro <horimoto@clear-code.com>
2023-10-12 22:36:57 +0900

    test: add path for testing tools

--- t/run-tests.sh
+++ t/run-tests.sh
@@ -10,7 +10,7 @@ ls -al /host/rakudo/share/perl6
 ls -al /host/rakudo/share/perl6/site
 ls -al /host/rakudo/share/perl6/site/bin
 
-export PATH=$PATH:/host/rakudo/share/perl6/site/bin
+export PATH=$PATH:/host/rakudo/bin:/host/rakudo/share/perl6/site/bin:/host/rakudo/share/perl6/vendor/bin:/host/rakudo/share/perl6/core/bin
 echo "Run tests!"
 prove6 --lib /host/t/
 echo "Finished tests!"
