3930050085066d5f3befd2af7c7948a813dfbb6d
Horimoto Yasuhiro <horimoto@clear-code.com>
2023-10-12 21:27:38 +0900

    Export test tool path

--- t/run-tests.sh
+++ t/run-tests.sh
@@ -10,6 +10,7 @@ ls -al /host/rakudo/share/perl6
 ls -al /host/rakudo/share/perl6/site
 ls -al /host/rakudo/share/perl6/site/bin
 
+export PATH=$PATH:/host/rakudo/share/perl6/site/bin
 echo "Run tests!"
-/host/rakudo/share/perl6/site/bin/prove6 --lib /host/t/
+prove6 --lib /host/t/
 echo "Finished tests!"
